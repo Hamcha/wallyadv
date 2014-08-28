6cbe0f8cb04f71e96cfb809b9ea0bc660da9d170
Hamcha <zikyky@gmail.com>
2014-08-28 22:55:29 +0200

    Goodbye lua files

--- main.lua
+++
@@ -1,9 +0,0 @@
--- Yep, this is just the hello world
-
-function love.draw()
-  love.graphics.print('WALLY.EXE TEST', 20, 20)
-end
-
-function love.keypressed(code)
-  if code == 1 then os.exit() end
-end
